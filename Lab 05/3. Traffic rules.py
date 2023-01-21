949643c944b590cffb6c17ddcda12386dc675f0d
Rohan Raj <rohan.raaj2@gmail.com>
2023-01-22 00:15:05 +0500

    Create 3. Traffic rules.py

---
+++ Lab 05/3. Traffic rules.py
@@ -0,0 +1,7 @@
+def traffic_message(color):
+ if color == "RED":
+ return "Vehicles must stop"
+ elif color == "GREEN":
+ return "Vehicles can move"
+ elif color == "YELLOW":
+ return "Vehicles should get ready to move"
