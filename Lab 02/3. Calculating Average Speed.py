c07a88d34ba95be8bc959a37f3c14a0c930dd172
Rohan Raj <rohan.raaj2@gmail.com>
2023-01-22 00:01:48 +0500

    Create 3. Calculating Average Speed.py

---
+++ Lab 02/3. Calculating Average Speed.py
@@ -0,0 +1,6 @@
+time_in_sec = 2562
+distance_in_mi = 6.21 # This value is obtained by rounding off the value
+obtained from 10/1.61 using: round(10/1.61, 2)
+time_in_hrs = time_in_sec/3600
+average_speed = distance_in_mi/time_in_hrs
+print(average_speed)
