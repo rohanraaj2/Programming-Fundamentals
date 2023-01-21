4c361f6cc09c9431ebe6d46ed440e09db410c45d
Rohan Raj <rohan.raaj2@gmail.com>
2023-01-22 00:02:17 +0500

    Create 4. Losing your marbles.py

---
+++ Lab 02/4. Losing your marbles.py
@@ -0,0 +1,6 @@
+marbles = 3000
+friends = 17
+marbles_per_friend = int(marbles/friends) # <--- Write your formula here.
+marbles_left_over = marbles - (marbles_per_friend * friends)
+print(marbles_per_friend)
+print(marbles_left_over)
