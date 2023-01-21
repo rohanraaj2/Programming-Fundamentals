6722f105c3d06d3568694dc4af7a394ebbc20d14
Rohan Raj <rohan.raaj2@gmail.com>
2023-01-22 00:13:14 +0500

    Create 6. Plus and Equals.py

---
+++ Lab 04/6. Plus and Equals.py
@@ -0,0 +1,15 @@
+def print_plus_and_equals(s):
+ number = int((s - 2) / 2)
+ space = " "
+ fullstars = s * "*"
+ print ((space * number) + ("*" * 2) + (space * number))
+ print ((space * number) + ("*" * 2) + (space * number))
+ print ("*" * s)
+ print ("*" * s)
+ print ((space * number) + ("*" * 2) + (space * number))
+ print ((space * number) + ("*" * 2) + (space * number))
+ print ("\n" * number)
+ print ("*" * s)
+ print (space)
+ print (space)
+ print ("*" * s)
